14c1f915941c099c525356914f3f8481c50bda3e
Bruno Gaignoux <gaignoux@gmail.com>
2021-05-28 23:58:27 -0300

    Modificando versão do vuesax

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <div class="selector">
         <div class="col-6">
           <vs-button
-            flat
+            type="border"
             :active="active == 0"
             @click="active = 0"
           >
@@ -14,7 +14,7 @@
         </div>
         <div class="col-6">
           <vs-button
-            flat
+            type="border"
             :active="active == 1"
             @click="active = 1"
           >
@@ -22,6 +22,144 @@
           </vs-button>
         </div>
       </div>
+    </vs-row>
+    <vs-row>
+      <vs-col vs-align="center" vs-type="flex" vs-justify="center" vs-w="8">
+        <vs-row>
+          <vs-col vs-align="center" vs-type="flex" vs-justify="center" vs-w="6">
+            <vs-card>
+              <div slot="header">
+                <h3>Pot with a plant</h3>
+              </div>
+              <div slot="media">
+                <img src="https://vuesax.com/foto13.png" alt="">
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p>
+              </div>
+              <div slot="footer">
+                <vs-button color="danger" icon>
+                  <box-icon name="heart"/>
+                </vs-button>
+                <vs-button class="btn-chat" shadow color="primary">
+                  <box-icon name="chat"/>
+                  <span class="span">
+                    54
+                  </span>
+                </vs-button>
+              </div>
+            </vs-card>
+          </vs-col>
+          <vs-col vs-align="center" vs-type="flex" vs-justify="center" vs-w="6">
+            <vs-card>
+              <div slot="header">
+                <h3>Pot with a plant</h3>
+              </div>
+              <div slot="media">
+                <img src="https://vuesax.com/foto6.png" alt="">
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p>
+              </div>
+              <div slot="footer">
+                <vs-button danger icon>
+                  <i class='bx bx-heart'></i>
+                </vs-button>
+                <vs-button class="btn-chat" shadow primary>
+                  <i class='bx bx-chat' ></i>
+                  <span class="span">
+                    54
+                  </span>
+                </vs-button>
+              </div>
+            </vs-card>
+          </vs-col>
+        </vs-row>
+        <vs-row>
+          <vs-col vs-align="center" vs-type="flex" vs-justify="center" vs-w="6">
+            <vs-card>
+              <div slot="header">
+                <h3>Pot with a plant</h3>
+              </div>
+              <div slot="media">
+                <img src="https://vuesax.com/foto12.png" alt="">
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p>
+              </div>
+              <div slot="footer">
+                <vs-button danger icon>
+                  <i class='bx bx-heart'></i>
+                </vs-button>
+                <vs-button class="btn-chat" shadow primary>
+                  <i class='bx bx-chat' ></i>
+                  <span class="span">
+                    54
+                  </span>
+                </vs-button>
+              </div>
+            </vs-card>
+          </vs-col>
+          <vs-col vs-align="center" vs-type="flex" vs-justify="center" vs-w="6">
+            <vs-card>
+              <div slot="header">
+                <h3>Pot with a plant</h3>
+              </div>
+              <div slot="media">
+                <img src="https://vuesax.com/foto5.png" alt="">
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p>
+              </div>
+              <div slot="footer">
+                <vs-button danger icon>
+                  <i class='bx bx-heart'></i>
+                </vs-button>
+                <vs-button class="btn-chat" shadow primary>
+                  <i class='bx bx-chat' ></i>
+                  <span class="span">
+                    54
+                  </span>
+                </vs-button>
+              </div>
+            </vs-card>
+          </vs-col>
+        </vs-row>
+      </vs-col>
+      <vs-col vs-align="center" vs-type="flex" vs-justify="center" vs-w="4">
+        <vs-card>
+          <div slot="header">
+            <h3>Próximas campanhas</h3>
+          </div>
+          <div>
+            <vs-list>
+              <vs-list-item title="One text"></vs-list-item>
+              <vs-list-item title="Another text" subtitle="A little text"></vs-list-item>
+              <vs-list-item title="Some more text"></vs-list-item>
+              <vs-list-item title="Even more text" subtitle="Another little text"></vs-list-item>
+            </vs-list>
+          </div>
+          <div slot="footer">
+            <vs-button danger icon>
+              <i class='bx bx-heart'></i>
+            </vs-button>
+            <vs-button class="btn-chat" shadow primary>
+              <i class='bx bx-chat' ></i>
+              <span class="span">
+                54
+              </span>
+            </vs-button>
+          </div>
+        </vs-card>
+      </vs-col>
     </vs-row>
   </div>
 </template>
@@ -37,12 +175,12 @@ export default {
     }
   },
   created () {
-    const loading = this.$vs.loading({
-      type: 'circles'
+    this.$vs.loading({
+      type: 'radius'
     })
 
     setTimeout(() => {
-      loading.close()
+      this.$vs.loading.close()
     }, 2000)
   }
 }
@@ -50,7 +188,7 @@ export default {
 <style lang="scss" scoped>
 .selector {
   position: relative;
-  left: 93%;
+  left: 47%;
   width: 120px;
   display: flex;
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
-  <vs-navbar class="default-background-color" text-white>
+  <vs-navbar class="default-background-color mb-10" collapse>
       <!-- <template #left>
         <vs-button icon border color="rgb(59,222,200)">
           <box-icon name="menu"/>
         </vs-button>
       </template> -->
-      <template #default>
-        <img src="../assets/logo.png" alt="logo" style="width: 5%; margin: 15px">
-      </template>
+      <div slot="title">
+        <vs-navbar-title>
+          Painel do Seller
+        </vs-navbar-title>
+      </div>
       <!-- <template #right>
         <box-icon name="exit"></box-icon>
       </template> -->
@@ -19,3 +21,17 @@ export default {
   name: 'Header'
 }
 </script>
+<style lang="scss">
+.mb-10 {
+  margin-bottom: 10px;
+  padding: 10px;
+
+  button span.btn-responsive-line {
+    background: #fff;
+  }
+
+  .vs-navbar--title {
+    color: #fff;
+  }
+}
+</style>
